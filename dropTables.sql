e191b2f740cb4f11ff3f7f8b3fc42f0b063c0bbb
Linlin Fan <100259051+gaizine@users.noreply.github.com>
2022-08-12 10:53:50 -0400

    drop tables sql

---
+++ dropTables.sql
@@ -0,0 +1,10 @@
+DROP TABLE Payment;
+DROP TABLE Reservation_Type;
+DROP TABLE Reservation;
+DROP TABLE Room;
+DROP TABLE Type;
+DROP TABLE Employee;
+DROP TABLE Location;
+DROP TABLE Brand;
+DROP TABLE Customer;
+DROP TABLE Person;
