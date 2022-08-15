6567c99cd063507fe98651ee2b88f0d28bf075fd
Linlin Fan <100259051+gaizine@users.noreply.github.com>
2022-08-15 18:45:09 -0400

    Update assignment2view.sql

--- assignment2view.sql
+++ assignment2view.sql
@@ -1,7 +1,9 @@
 --senario 1: the room types population rank --
 create view popularType as
 select typeID, sum(endDate - startDate) as dateSum 
-from RESERVATION_TYPE
+from RESERVATION_TYPE left join RESERVATION
+on RESERVATION_TYPE.typeID = RESERVATION.typeID
+WHERE reservation.reservationStatus = 1
 group by typeID 
 order by sum(endDate - startDate) DESC ;
 select * from popularType;
@@ -14,14 +16,17 @@ on room.locationID = location.locationID;
 
 
 --senario 2: the room type of earning money rank --
-create view earningType as
+--create view earningType as
 select reservation_type.typeID, sum((reservation_type.endDate - reservation_type.startDate)*(reservation_type.price - type.maintainCost)) as earn
 from reservation_type left join type
 on reservation_type.typeID = type.typeID
+left join RESERVATION
+on RESERVATION_TYPE.typeID = RESERVATION.typeID
+where reservation.reservationstatus = 1
 group by reservation_type.typeid
 order by sum((reservation_type.endDate - reservation_type.startDate)*(reservation_type.price - type.maintainCost));
 
-drop view earningType;
+--drop view earningType;
 
 --create view earningRank as
 select * from earningType inner join room
@@ -32,18 +37,21 @@ on room.locationID = location.locationID;
 
 
 ---senario 3: count the employee in every location
+select locationID, count(employeeID)
+from employee_location
+group by locationID;
 
 
-
---senario 4: order seasonal change 
-select count(*) from reservation_type group by EXTRACT(MONTH FROM reservation_type.startDate);
+--senario 4: order seasonal trend 
+--select count(*) from reservation_type group by EXTRACT(MONTH FROM reservation_type.startDate);
 --senario 4: order seasonal change 
 select sum((reservation_type.endDate - reservation_type.startDate)*(reservation_type.price - type.maintainCost)) as "earn", 
 EXTRACT(MONTH FROM reservation_type.startDate) as "month"
 from reservation_type left join type
 on reservation_type.typeID = type.typeID
+left join reservation
+on reservation_type.reservationid = reservation.reservationid
+where reservation.reservationstatus = 1
 group by EXTRACT(MONTH FROM reservation_type.startDate)
 order by sum((reservation_type.endDate - reservation_type.startDate)*(reservation_type.price - type.maintainCost)) desc;
-
-
 
